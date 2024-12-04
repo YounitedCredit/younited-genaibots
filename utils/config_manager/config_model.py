7b7e1a86fd265390d909bd3846159e96f4c683e7
ANTOINE HABERT <antoine.habert@younited-credit.fr>
2024-12-04 13:54:36 +0100

    changed incorrect description

--- utils/config_manager/config_model.py
+++ utils/config_manager/config_model.py
@@ -50,7 +50,7 @@ class BotConfig(BaseModel):
     # The default plugin for instant messaging behavior, which defines how the bot behaves in IM platforms (e.g., Slack or Teams).
     USER_INTERACTIONS_INSTANT_MESSAGING_BEHAVIOR_DEFAULT_PLUGIN_NAME: str
 
-    # Specify an override that force the bot to treat all comments as internal comments.
+    # Specify an override that force the bot to disable comment message in outputs.
     DISABLE_COMMENT: str
 
     # The default plugin for session management
