15a2621a8c980dc38c20e2cd327bb4182111d116
ANTOINE HABERT <antoine.habert@younited-credit.fr>
2025-02-05 16:49:56 +0100

    updated auto install of requirements with upgrade

--- install_requirements.py
+++ install_requirements.py
@@ -1,13 +1,12 @@
 import os
 import subprocess
 
-
 def install_requirements():
     for root, dirs, files in os.walk('.'):
         for file in files:
             if file == 'requirements.txt':
                 file_path = os.path.join(root, file)
-                subprocess.run(['pip', 'install', '-r', file_path], check=True)
+                subprocess.run(['pip', 'install', '--upgrade', '-r', file_path], check=True)
 
 if __name__ == '__main__':
     install_requirements()
